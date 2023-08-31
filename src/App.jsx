cde4e9ff6bd113afe287e905a0ee2197dcde62b2
nii_okantey <nunoogideon19@gmail.com>
2023-08-31 13:52:50 +0000

    updates

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path="" element={<Navbar />} />
+      </Routes>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      cabin: ["Cabin", "sans-serif"],
+      lato: ["lato", "sans-serif"],
+    },
+    backgroundImage: {
+      "hero": "url('https://img.freepik.com/free-vector/auto-mechanicconcept-illustration_114360-16748.jpg?w=740&t=st=1693488590~exp=1693489190~hmac=f9804c009ddfde0f2cc5072a36af4f72f1390464cf3594c0e32cf71f4847e202')",
+      "footer-texture": "url('/img/footer-texture.png')",
+    },
+    extend: {
+      colors: {
+        primary: "#A14189",
+        gray: "#9C9C9C",
+      },
+      container: {
+        // padding: {
+        // 	DEFAULT: "1rem",
+        // 	sm: "1rem",
+        // 	lg: "4rem",
+        // 	xl: "5rem",
+        // 	"2xl": "6rem",
+        // },
+        center: true,
+      },
+    },
+  },
+  plugins: [],
+};
