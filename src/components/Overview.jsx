ce10910b3566c7ed2e81a342f26fbc9c677fbf0f
nii_okantey <nunoogideon19@gmail.com>
2023-09-02 17:26:49 +0000

    updates

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Overview = () => {
-  return <div>Overview</div>;
+  return (
+    <div className="my-12">
+      <div className="w-full m-auto text-center md:w-[55%]">
+        <h1 className="text-2xl font-bold">How KwansoBoafo Works.</h1>
+        <p className="my-4 text-gray">
+          Download and install the tere app. Enter your phone number and make
+          your user account. when approved you may start requesting mechanics.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Overview;
