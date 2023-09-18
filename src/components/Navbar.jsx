90b2224102dc51df90ce9fad94719c3807e5110e
nii_okantey <nunoogideon19@gmail.com>
2023-09-18 17:14:05 +0000

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,6 @@ const Navbar = () => {
     <nav className="bg-[whitesmoke] w-full flex justify-between items-center sticky top-0 left-0 right-0 z-10  ">
       <Brand />
       <div className="hidden md:flex md:items-center md:gap-6">
-        <Link className="text-base hover:text-primary">Home</Link>
         <Link className="text-base hover:text-primary">Service Provider</Link>
         <Link className="text-base hover:text-primary">About Us</Link>
         <Link className="text-base hover:text-primary">Contact</Link>
